ca09baccd8978a128f964e9cd8c2dcecf587e513
“zhouyang <zhouyang@njdyk.cn>
2023-03-16 16:49:10 +0800

    node16 适配

--- vue.config.js
+++ vue.config.js
@@ -78,7 +78,7 @@ module.exports = {
       scss: {
         // 向全局sass样式传入共享的全局变量, $src可以配置图片cdn前缀
         // 详情: https://cli.vuejs.org/guide/css.html#passing-options-to-pre-processor-loaders
-        prependData: `
+        additionalData: `
           @import "assets/css/mixin.scss";
           @import "assets/css/variables.scss";
           $cdn: "${defaultSettings.$cdn}";
